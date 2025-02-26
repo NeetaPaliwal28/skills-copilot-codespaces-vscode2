9bfd08d38a035946ab88a3fbde043a2ed117c679
NeetaPaliwal28 <130063697+NeetaPaliwal28@users.noreply.github.com>
2025-02-26 10:44:50 +0530

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,4 +1,20 @@
-//Comment new View 
-app.get('/comments/new', function(req, res) {
-  res.render('comments/new');
+// Create web server
+const express = require('express');
+const app = express();
+// Set up express to use ejs
+app.set('view engine', 'ejs');
+// Set up express to serve static files
+app.use(express.static('public'));
+
+// Import the comments data
+const comments = require('./data/comments');
+
+// Create a route for the comments
+app.get('/comments', (req, res) => {
+    res.render('comments', { comments });
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started');
 });
